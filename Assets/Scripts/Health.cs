3b658b1475f8659311b00afe4e2e10d7ed5c31b4
kaitlynkilner <kaitlynkilner@ufl.edu>
2024-04-06 19:22:12 -0400

    Added health mechanic

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
@@ -8,13 +9,16 @@ public class Health : MonoBehaviour
     [SerializeField] private GameObject player;
     private float timer = 10f;
     [SerializeField] private float health;
-    private float maxHealth = 10f;
+    private float maxHealth = 100f;
     private bool ifExited = false;
+    private bool ifRaining = false;
+    [SerializeField]public Image healthBar;
     // Start is called before the first frame update
     void Start()
     {
         rain.SetActive(false);
         health = maxHealth;
+
     }
 
     // Update is called once per frame
@@ -25,26 +29,38 @@ public class Health : MonoBehaviour
         if(timer <= 0f){
             rain.SetActive(false);
             timer = 10f;
+            ifExited = true;
+            ifRaining = false;
         }
-        if(ifExited && timer > 1f){
-            TakeDamage(.5f);
+        if(ifExited && timer > 1f && ifRaining){
+            TakeDamage(.1f);
         }
 
     }
 
     private void OnTriggerEnter2D(Collider2D col){
+        if((col.gameObject.name == "UnderPlatformTrigger" || col.gameObject.name == "UnderPlatformTrigger(1)") && ifRaining){
+            Debug.Log("Under Platform");
+            ifExited = false;
+        }
+        if(col.gameObject.name == "Acid_rainTrigger"){
         Debug.Log("Collision Detected");
         rain.SetActive(true);
+        ifExited = true;
+        ifRaining = true;
+        col.gameObject.SetActive(false);
+        }
     }
-    private void OnTriggerExit2D(Collider2D col){
+     private void OnTriggerExit2D(Collider2D col){
         Debug.Log("Exited");
         ifExited = true;
     }
 
     private void TakeDamage(float ammount){
         health -= ammount;
+        healthBar.fillAmount = health / maxHealth;
         if(health <= 0f){
             player.SetActive(false);
         }
         }
-}
+    }
