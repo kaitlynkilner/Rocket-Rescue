65fc94a61e39bcda7df0047241c5b72c0e52e6fd
kaitlynkilner <kaitlynkilner@ufl.edu>
2024-04-05 22:33:59 -0400

    Update

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private float jump = 16f;
+    private bool facingRight = true;
+    // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if(Input.GetButtonDown("Jump") && canJump()){
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f){
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y *.5f);
+        }
+        Flip();
+        
+    }
+
+    private void FixedUpdate(){
+        rb.velocity = new Vector2(horizontal*speed, rb.velocity.y);
+    }
+
+    private void Flip(){
+        if(facingRight && horizontal < 0f || !facingRight && horizontal > 0f){
+            facingRight = !facingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+    
+    private bool canJump(){
+        return Physics2D.OverlapCircle(groundCheck.position, .2f, groundLayer);
+    }
+}
