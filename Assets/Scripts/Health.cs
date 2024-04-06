94fb9e13d995ad7489a0e77bf42e194462570e28
kaitlynkilner <kaitlynkilner@ufl.edu>
2024-04-06 17:21:15 -0400

    Update

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private GameObject rain;
+    [SerializeField] private GameObject player;
     private float timer = 10f;
+    [SerializeField] private float health;
+    private float maxHealth = 10f;
+    private bool ifExited = false;
     // Start is called before the first frame update
     void Start()
     {
         rain.SetActive(false);
+        health = maxHealth;
     }
 
     // Update is called once per frame
@@ -19,12 +24,27 @@ public class Health : MonoBehaviour
             timer -= Time.deltaTime;}
         if(timer <= 0f){
             rain.SetActive(false);
+            timer = 10f;
         }
-        
+        if(ifExited && timer > 1f){
+            TakeDamage(.5f);
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D col){
         Debug.Log("Collision Detected");
         rain.SetActive(true);
     }
+    private void OnTriggerExit2D(Collider2D col){
+        Debug.Log("Exited");
+        ifExited = true;
+    }
+
+    private void TakeDamage(float ammount){
+        health -= ammount;
+        if(health <= 0f){
+            player.SetActive(false);
+        }
+        }
 }
